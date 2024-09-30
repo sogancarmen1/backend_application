7899e1423102ff3f100d08ca4d58ca36b6cc956e
sogancarmen1 <sogancarmen1@gmail.com>
2024-10-01 00:11:46 +0100

    change again app file

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ class App {
       })
     );
     this.app.all("/*", function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
       res.header(
         "Access-Control-Allow-Methods",
         "GET, PUT, POST, DELETE, OPTIONS"
