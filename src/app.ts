679c1a769fa7996bee2cf48fbea55147e0250b41
sogancarmen1 <sogancarmen1@gmail.com>
2024-10-06 16:06:08 +0100

    change link in cors

--- src/app.ts
+++ src/app.ts
@@ -29,14 +29,16 @@ class App {
     this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
     this.app.use(
       cors({
-        origin: "https://front-end-to-do-list-niul.vercel.app",
+        // origin: "https://front-end-to-do-list-niul.vercel.app",
+        origin: "http://localhost:3000",
         credentials: true,
       })
     );
     this.app.all("/*", function (req, res, next) {
       res.header(
         "Access-Control-Allow-Origin",
-        "https://front-end-to-do-list-niul.vercel.app"
+        "http://localhost:3000"
+        // "https://front-end-to-do-list-niul.vercel.app"
       );
       res.header(
         "Access-Control-Allow-Methods",
